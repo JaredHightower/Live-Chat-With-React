b3784b4136c3a2a8fd22dd87047ecbdc7ab932c3
Jared Hightower <jared.jhightower@gmail.com>
2021-11-15 11:58:38 -0600

    Firstbranch (#1)
    * First Change to App.js and Index.js
    
    * updated firebase files
    
    * updated firebase files
    
    * added and test firestore connection
    
    * pulling data form firebase
    
    * add realtime updates from firestore database
    
    * add realtime updates from firestore database
    
    * updated useEffect
    
    * updated useEffect
    
    * updated firebase hosting
    
    * updated firebase hosting
    
    * updated firebase hosting
    
    * updated firebase hosting
    
    * fix error
    
    * removed yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, onSnapshot } from "firebase/firestore";
+
+const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyAbBTNXgod0UkTMsgIxjupgfBk3YFYQ8jk",
+  authDomain: "chat-app-66901-dev.firebaseapp.com",
+  databaseURL: "https://chat-app-66901-dev-default-rtdb.firebaseio.com",
+  projectId: "chat-app-66901-dev",
+  storageBucket: "chat-app-66901-dev.appspot.com",
+  messagingSenderId: "556943717887",
+  appId: "1:556943717887:web:4be09fd9ccb6425cade3ca",
+});
+
+const db = getFirestore(firebaseConfig);
 
 function App() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "channels"), (snapshot) => {
+        setChannels(
+          snapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }))
+        );
+      }),
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Nav">
+        <div className="User">
+          <img
+            className="UserImage"
+            alt="whatever"
+            src="https://placekitten.com/64/64"
+          />
+          <div>
+            <div>Jared Hightower</div>
+            <div>
+              <button className="text-button">log out</button>
+            </div>
+          </div>
+        </div>
+        <nav className="ChannelNav">
+          {channels.map((channel) => (
+            <a href={`/channel/${channel.id}`}># {channel.id}</a>
+          ))}
+        </nav>
+      </div>
+      <div className="Channel">
+        <div className="ChannelMain">
+          <div className="ChannelInfo">
+            <div className="Topic">
+              Topic: <input className="TopicInput" value="Awesome stuff" />
+            </div>
+            <div className="ChannelName">#general</div>
+          </div>
+          <div className="Messages">
+            <div className="EndOfMessages">That's every message!</div>
+            <div>
+              <div className="Day">
+                <div className="DayLine" />
+                <div className="DayText">5/6/2021</div>
+                <div className="DayLine" />
+              </div>
+              <div className="Message with-avatar">
+                <div className="Avatar" />
+                <div className="Author">
+                  <div>
+                    <span className="UserName">Jared Hightower </span>
+                    <span className="TimeStamp">3:37 PM</span>
+                  </div>
+                  <div className="MessageContent">Alright, lets do this.</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="Message no-avatar">
+                <div className="MessageContent">works now?</div>
+              </div>
+            </div>
+          </div>
+          <div className="ChatInputBox">
+            <input className="ChatInput" placeholder="Message #general" />
+          </div>
+        </div>
+        <div className="Members">
+          <div>
+            <div className="Member">
+              <div className="MemberStatus offline" />
+              Jared Hightower
+            </div>
+            <div className="Member">
+              <div className="MemberStatus online" />
+              cleverbot
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
